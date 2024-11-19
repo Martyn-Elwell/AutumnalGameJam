5c483937ca9147f3033be99c09258e8ebc702fc0
George Betts <george2.betts@live.uwe.ac.uk>
2024-11-19 12:20:41 +0000

    You can now drag pickup into the level

--- Source/AutumnalGameJam/Private/Pickup.cpp
+++ Source/AutumnalGameJam/Private/Pickup.cpp
@@ -9,7 +9,7 @@ APickup::APickup()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-    StaticMesh = CreateDefaultSuboject<UStaticMeshComponent>(TEXT("Static Mesh"));
+    StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
     RootComponent = StaticMesh;
 
 }
--- Source/AutumnalGameJam/Public/Pickup.h
+++ Source/AutumnalGameJam/Public/Pickup.h
@@ -24,6 +24,6 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
     UPROPERTY(EditAnywhere)
-    class UStaticMeshComponent StaticMesh;
+    class UStaticMeshComponent* StaticMesh;
 
 };
